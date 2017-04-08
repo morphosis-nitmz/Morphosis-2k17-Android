7d66e7fa21dc84e2ae934bc6034e52cc060560c1
codegeek96 <thegeek.004@gmail.com>
2017-04-08 17:20:29 +0530

    Changes in profile page.

--- app/src/main/java/com/nitmz/morphosis/scoobydoo/ProfileFragment.java
+++ app/src/main/java/com/nitmz/morphosis/scoobydoo/ProfileFragment.java
@@ -6,17 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.nitmz.morphosis.CircleImageView;
 import com.nitmz.morphosis.R;
 
+
 public class ProfileFragment extends Fragment {
 
     private FirebaseAuth mAuth;
 
     private CircleImageView mProfileImage;
+    private TextView mUserName;
+    private TextView mRank;
+    private TextView mScore;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,10 +42,18 @@ public class ProfileFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
 
         mProfileImage = (CircleImageView) view.findViewById(R.id.profile_image_view);
-        Glide.with(this).
-                load(mAuth.getCurrentUser().getPhotoUrl().toString()).
-                fitCenter().
-                dontAnimate().
-                into(mProfileImage);
+        mUserName = (TextView) view.findViewById(R.id.name_text_view);
+        mRank = (TextView) view.findViewById(R.id.rank_text_view);
+        mScore = (TextView) view.findViewById(R.id.score_text_view);
+
+        Glide.with(this)
+                .load(mAuth.getCurrentUser().getPhotoUrl().toString())
+                .fitCenter()
+                .dontAnimate()
+                .into(mProfileImage);
+
+        mUserName.setText(mAuth.getCurrentUser().getDisplayName());
+        mUserName.setTextSize(45);
+        mUserName.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
     }
 }
