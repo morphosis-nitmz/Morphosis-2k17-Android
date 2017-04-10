29ff197073d5cbbe08ad6da4582fac4c1c8d5e01
anr007 <narayanareddy500@gmail.com>
2017-04-10 19:38:06 +0530

    Resolved conflicts with the remote branch

---
+++ app/src/main/java/com/nitmz/morphosis/scoobydoo/GameStatusActivity.java
@@ -0,0 +1,15 @@
+package com.nitmz.morphosis.scoobydoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nitmz.morphosis.R;
+
+public class GameStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_status);
+    }
+}
--- app/src/main/java/com/nitmz/morphosis/scoobydoo/LeaderBoardUserDetailsFragment.java
+++ app/src/main/java/com/nitmz/morphosis/scoobydoo/LeaderBoardUserDetailsFragment.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.nitmz.morphosis.CircleImageView;
 import com.nitmz.morphosis.R;
 
 
