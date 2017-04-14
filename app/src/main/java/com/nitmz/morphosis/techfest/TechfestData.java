9afda669cbfd0a4946e2bb58820911eb63454fff
codegeek96 <thegeek.004@gmail.com>
2017-04-15 02:14:34 +0530

    Create about page for some events.

--- app/src/main/java/com/nitmz/morphosis/techfest/TechfestData.java
+++ app/src/main/java/com/nitmz/morphosis/techfest/TechfestData.java
@@ -3,121 +3,16 @@ package com.nitmz.morphosis.techfest;
 
 public class TechfestData {
 
-    String[] aboutEvent = {
-            "The share markets and their ruckus has a lot to do with our daily lives. " +
-                    "The fluctuations there make or break our sleeps. " +
-                    "Keeping in view the real thrill of the market monotony, " +
-                    "we are here with the Virtual Simulation of the same thrill and " +
-                    "adrenaline rush though the event under the banner of MORPHOSIS. " +
-                    "The virtual share market enables the participants to trade, buy, sell, " +
-                    "mortgage and showcase their rationality, their mettle against competitive " +
-                    "decision making under pressure. The full time active virtual market " +
-                    "will be a platform to showcase your inferential, pressure-handling capability. " +
-                    "So, be a part of it to experience the Raising Spirit of Competitiveness " +
-                    "and explore your ability of Decision Making.",
-
-            "Not an usual, stereotypical one, but this with a huge base of stipulations based on " +
-                    "multiple rounds, pertaining to vast realms of knowledge will let the " +
-                    "participants to test their knowledge and also will be a driving force to " +
-                    "workout the mental muscles. Provoking teasers from various areas viz. " +
-                    "Biology, Physics, Chemistry, General Science, General Knowledge, Logical " +
-                    "Puzzles and many more will surely test the audacious minds of quiz-takers " +
-                    "and nevertheless would be a super-brain-teaser-activity. So stop thinking " +
-                    "and be a part of it to experience the brain-storm in MORPHOSIS!",
-
-            "Arcadia",
-
-            "One of the most indulging, amazing and captivating inventions ever done by humans " +
-                    "is the Rubik's Cube. It's endless number of combinations have allured the " +
-                    "humans for time immemorial. MORPHOSIS is the platform where you will get a " +
-                    "chance to rework your neural muscles against a plethora of possible " +
-                    "combinations. The competition is a fusion of fastest fingers fast and " +
-                    "adrenaline rush to solve the cube in the shortest possible span of time, " +
-                    "thereby channelizing the speed of thinking through the fingers. Let your " +
-                    "finger do the talking in this MORPHOSIS !",
-
-            "Welcome to the Land of Wisdom where your curiosity, smartness and wisdom will help " +
-                    "you unravel many mysteries, facts and profound knowledge. \"Perhaps when a " +
-                    "man has special knowledge and special powers, it rather encourages him to " +
-                    "seek a complex explanation when a simpler one is at hand.\", yet we suggest " +
-                    "you keep it simple. Be aware cause you will be facing challenges in two stages:\n" +
-                    "One being the random questionnaire testing your knowledge and googling skill.\n" +
-                    "Other being the mystery story trickling your brains where enigmatic facts, " +
-                    "mythology and mind boggling puzzles will test your skillful knowledge, " +
-                    "awareness, decisive mind to guide you to the greatest treasure. So, get " +
-                    "ready for the Treasure Hunt, only in MORPHOSIS !",
-
-            "\"Practice makes a man perfect\". It is not an exception for the interviews! The " +
-                    "event mimics the phases of interview in fetching a job thereby creating an " +
-                    "atmosphere of placement season. Through this event, MORPHOSIS nurture the " +
-                    "mindset of participants  for the upcoming placement season. The virtual " +
-                    "interview process drills the participant through a number of rounds thereby " +
-                    "providing them mantras for cracking the job interviews. This event is meant " +
-                    "to inculcate the DOs and DONT's for getting dream job offers.",
-
-            "The ability to express your views on a topic is one of the most influential trait " +
-                    "of one's personality. This event presents you with an opportunity to express " +
-                    "your opinions, to display your refutal and rebuttal skills in presence of " +
-                    "acclaimed judges. Apart from being a thriving opportunity for the audacious " +
-                    "orators out there, it would also prove to be an apt panacea for the " +
-                    "not-so-grooved orators,who possess tidbits of knowledge on some of the " +
-                    "most burning topics nowadays. So, get your knowledge orchard ready to sell " +
-                    "the fruits at MORPHOSIS.",
-
-            "The thrilling coding megathlon is an assured nerve-wrecking activity for the geeks " +
-                    "out there. MORPHOSIS, in the form of of Code Warrior, provides the platform " +
-                    "to showcase the problem solving skills. Participants get the opportunity to " +
-                    "fetch the title of \"Mr/Ms Algorithmist\" by outperforming the best coders " +
-                    "of India. Code Warrior gives exposure to exhibit programming skills under " +
-                    "time constraints while competing against the best minds of the nation. So, " +
-                    "get ready with cups of coffee to crack the code in this MORPHOSIS!",
-
-            "With an unique and catchy name, it surely catches the acclaimed attention of the " +
-                    "masses with it's highly provoking and thoughtful questions on Quantitative " +
-                    "Aptitude, Data Interpretation and Logical Reasoning. The event has two " +
-                    "phases for testing both the objective  and subjective question solving " +
-                    "skills. Cypher, apart from testing the reasoning and mathematical skills, " +
-                    "provides an opportunity to brush-up the short-cut techniques and heuristic " +
-                    "approaches for reaching at the solution. Also, it directs the participants " +
-                    "to the unique world of aptitude that awaits your attention at some point of " +
-                    "your academic career. Exhibit you mathematics skills in this MORPHOSIS!",
-
-            "\"The expressing ability of a person defines his personality\". MORPHOSIS brings to " +
-                    "you an excellent opportunity to showcase your presentation skills. Tech " +
-                    "Talk comes to aid those who get nervous and stumble upon hearing the words " +
-                    "\"Present You !\". Tech Talk challenges you to present your on a " +
-                    "technical/non-technical domain in the form of extempore speech or a power " +
-                    "presentations, Q & A sessions, Research Paper description, talking about " +
-                    "trending technology and much more. Tech Talk aims at providing an excellent " +
-                    "chance to train the communicational mettle of the participants and help  " +
-                    "them get over the highly common fear of public speaking and presentations. " +
-                    "Sync your thought process with your expressing ability and reinforce your " +
-                    "objective to meet our expectation in this MORPHOSIS.",
-
-            "MORPHOSIS brings to you the opportunity to prove the \"Scientist in You\". In the " +
-                    "fast moving technological world, the need of application of acquired " +
-                    "knowledge is at an all time high. Keeping this in mind, this event " +
-                    "encourages everyone to show their innovative side to the world. Science " +
-                    "Expo acts as a bridge to connect the youth to the STEM (Science, Technology, " +
-                    "Engineering, Mathematics) opportunities. The models and various inventions " +
-                    "are welcomed to be show cast on this acclaimed platform. It will be a major " +
-                    "source of inspiration to the budding innovators and also to those having a " +
-                    "deep sense of innovation slumbering within them.",
-
-            "S-Marketing alias Smart Marketing is a unique and frolicking opportunity to test " +
-                    "the managerial skills of the participants. Coupled with Digital Marketing, " +
-                    "S-Marketing has it's own flavors to test the ability of the participants in " +
-                    "the capacity of a salesperson. MORPHOSIS expands it's outreach to Management " +
-                    "field to let the participants exhibit their marketing & promotional skills. " +
-                    "An instance of this event would requires you to sell a product to a number."
-    };
-
     int[] rulesPages = {
             2, 2, 3, 0, 1, 6, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0
     };
 
-    public String getAboutEvent(int position) {
-        return aboutEvent[position];
+    int[] aboutPage = {
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1
+    };
+
+    public int getAboutEvent(int position) {
+        return aboutPage[position];
     }
 
     public int getRulesPage(int position) {
