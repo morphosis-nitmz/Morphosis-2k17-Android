a3886680586d07d540bc8e18f11adb948aedce95
codegeek96 <thegeek.004@gmail.com>
2017-04-19 00:38:49 +0530

    Create Developer Activity

--- app/src/main/java/com/nitmz/morphosis/techfest/DevelopersActivity.java
+++ app/src/main/java/com/nitmz/morphosis/techfest/DevelopersActivity.java
@@ -2,14 +2,41 @@ package com.nitmz.morphosis.techfest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.nitmz.morphosis.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DevelopersActivity extends AppCompatActivity {
+
+    List<String> mWebTeam;
+
+    ListView mWebTeamList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developers);
+
+        mWebTeam = new ArrayList<>(Arrays.asList(
+                "Akash Singh",
+                "Saurav Saha",
+                "Sunil Kumar",
+                "Jereemi Bentham",
+                "Sunday Lalbiaknia",
+                "Shantanu Acharya",
+                "Anki Reddy Narayana Reddy"
+        ));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                mWebTeam);
+
+        mWebTeamList = (ListView) findViewById(R.id.web_team_list_view);
+        mWebTeamList.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/nitmz/morphosis/techfest/TechfestSplashScreenActivity.java
+++ app/src/main/java/com/nitmz/morphosis/techfest/TechfestSplashScreenActivity.java
@@ -7,8 +7,10 @@ import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nitmz.morphosis.LoginActivity;
 import com.nitmz.morphosis.R;
@@ -28,29 +30,30 @@ public class TechfestSplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scooby_doo_splash_screen);
 
-        if (Build.VERSION.SDK_INT >= 21) {
+        /*if (Build.VERSION.SDK_INT >= 21) {
             Window window = getWindow();
             //window.setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
             window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
-        }
+        }*/
 
         mMorphosisTitle = (TextView) findViewById(R.id.about_title_splash_screen);
         mMorphosisSubTitle = (TextView) findViewById(R.id.about_sub_title_splash_screen);
 
-        Typewriter writer = new Typewriter(this);
-        setContentView(writer);
+        //Typewriter writer = new Typewriter(this);
+        //setContentView(writer);
 
         //Add a character every 150ms
-        writer.setCharacterDelay(SPLASH_TIME_OUT);
-        writer.animateText("Yello");
-        checkLoginStatus();
+        //writer.setCharacterDelay(SPLASH_TIME_OUT);
+        //writer.animateText("Yello");
+        //checkLoginStatus();
 
-        /*new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 checkLoginStatus();
+                Toast.makeText(TechfestSplashScreenActivity.this, "Hello", Toast.LENGTH_LONG).show();
             }
-        }, SPLASH_TIME_OUT); */
+        }, SPLASH_TIME_OUT);
     }
 
     private void checkLoginStatus() {
@@ -58,15 +61,13 @@ public class TechfestSplashScreenActivity extends AppCompatActivity {
         boolean  logIn = status.getBoolean("in", false);
         if (logIn) {
             Intent intent = new Intent(TechfestSplashScreenActivity.this, TechfestHomeActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
             startActivity(intent);
             finish();
         } else {
             Intent intent = new Intent(TechfestSplashScreenActivity.this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
             intent.putExtra("launch", 2);
             startActivity(intent);
+            finish();
         }
-        finish();
     }
 }
